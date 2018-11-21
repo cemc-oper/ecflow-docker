9c96bac379bad8adb5d8e904da165c6f18f7a7b5
perillaroc <perillaroc@gmail.com>
2018-11-21 18:55:05 +0800

    :whale: add support for getting codes from internet.

---
+++ docker/base/get_code.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -ex
+cd /tmp
+boost_file_name=boost_${BOOST_VERSION}.tar.gz
+
+cd /tmp
+test -d ecflow_code || mkdir ecflow_code
+cd ecflow_code
+
+if [ ! -f ${boost_file_name} ]; then
+    boost_version_dot=$(echo ${BOOST_VERSION} | sed "s/_/\./g")
+    boost_url=https://jaist.dl.sourceforge.net/project/boost/boost/${boost_version_dot}/boost_${BOOST_VERSION}.tar.gz
+    wget ${boost_url}
+fi
+cp ${boost_file_name} /tmp
+
+ecflow_file_name=ecFlow-${ECFLOW_VERSION}-Source.tar.gz
+
+if [ ! -f ${ecflow_file_name} ]; then
+    wget https://confluence.ecmwf.int/download/attachments/8650755/${ecflow_file_name}
+fi
+cp ${ecflow_file_name} /tmp
